6480a4a713eaf49e42f878bb23a6e9c3e6f2fce5
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2026-02-13 03:31:12 -0500

    Code Quality Fixes | Minor Bug Fixes
    - Reduce use of is_some(), is_none() etc.
    - Uses enum for Input types
    - Move read_n_lines, follow_filter to impl FileSpec { }
    - Fix empty line prints with -n
    - Start at correct offset for stdin

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{
     fs::File,
     io::{IsTerminal, Read, Seek, SeekFrom},
     path::Path,
+    process,
     str::from_utf8,
 };
 
@@ -11,16 +12,20 @@ use notify::{RecursiveMode, Watcher};
 
 use static_str::to_str;
 
+enum Input {
+    File(&'static Path),
+    Stdin,
+}
+
 struct FileSpec {
     size: u64,
-    fpath: Option<&'static Path>,
-    stdin: Option<Vec<String>>,
+    input: Input,
 }
 
 impl FileSpec {
-    fn new(fpath: Option<&'static Path>, stdin: Option<Vec<String>>) -> Self {
+    fn new(input: Input) -> Self {
         let size: u64 = 0;
-        let mut ret = Self { fpath, size, stdin };
+        let mut ret = Self { input, size };
         ret.update_size();
         ret
     }
@@ -28,39 +33,139 @@ impl FileSpec {
     // size_on_disk() wasn't returning actual file size for linux.
     #[cfg(any(target_os = "linux", target_os = "macos"))]
     fn update_size(&mut self) {
-        self.size = if self.fpath.is_some() {
-            self.fpath.unwrap().metadata().unwrap().len()
-        } else {
-            0
-        }
-        // self.size = self.fpath.unwrap().metadata().unwrap().len();
+        self.size = match self.input {
+            Input::File(fpath) => fpath.metadata().unwrap().len(),
+            _ => 0,
+        };
     }
 
     #[cfg(target_os = "windows")]
     fn update_size(&mut self) {
-        self.size = self.fpath.unwrap().size_on_disk().unwrap();
+        self.size = match self.input {
+            Input::File(fpath) => fpath.unwrap().size_on_disk().unwrap(),
+            _ => 0,
+        }
+    }
+
+    fn read_last_n_lines(&mut self, num: i32) {
+        let mut b_ns = num;
+        let mut start: u64 = 0;
+
+        match self.input {
+            Input::File(path) => {
+                let mut f = File::options().read(true).write(false).open(path).unwrap();
+
+                // Stop when number of \n are met, or the file is completely read.
+                while b_ns >= 0 && start < self.size {
+                    // Read one byte at a time until we reach the specified number of \n's (b_ns)
+                    start = start + 1;
+
+                    f.seek(SeekFrom::Start(self.size - start)).unwrap();
+
+                    let mut buf = vec![0; 1];
+                    f.read_exact(&mut buf).unwrap();
+
+                    if from_utf8(&buf).unwrap() == "\n".to_string() {
+                        b_ns -= 1;
+                    }
+                }
+                f.seek(SeekFrom::Start(self.size - start + 1)).unwrap();
+
+                let mut buf_print = Vec::new();
+                f.read_to_end(&mut buf_print).unwrap();
+
+                print!("{}", String::from_utf8(buf_print).unwrap());
+            }
+            Input::Stdin => {
+                if !std::io::stdin().is_terminal() {
+                    let mut buf_print = String::new();
+                    let stdin_lines = std::io::stdin();
+
+                    for lines in stdin_lines
+                        .lines()
+                        .collect::<Result<Vec<_>, _>>()
+                        .unwrap()
+                        .iter()
+                        .rev()
+                    {
+                        buf_print.insert_str(0, &format!("{}\n", lines)[..]);
+                        b_ns -= 1;
+                        if b_ns < 0 {
+                            break;
+                        }
+
+                        print!("{}", buf_print);
+                    }
+                }
+            }
+        };
+    }
+
+    fn follow_filter(&mut self, filter: &str) {
+        match self.input {
+            Input::File(fpath) => {
+                if fpath.metadata().unwrap().len() >= self.size {
+                    // Regular tail -f behaviour so far.
+                    let mut f = File::options().read(true).write(false).open(fpath).unwrap();
+
+                    // delay updating self.size so that we know exact seek of where the last output ended
+                    f.seek(SeekFrom::Start(self.size)).unwrap();
+
+                    let mut buf = Vec::new();
+                    f.read_to_end(&mut buf).unwrap();
+                    let new_line = String::from_utf8(buf).unwrap();
+
+                    // Start filtering things out here...
+                    let lines = new_line.split("\n");
+
+                    for line in lines {
+                        if line.contains(&filter) {
+                            let phrases: Vec<&str> = line.split(filter).collect();
+                            for i in phrases[..phrases.len() - 1].iter() {
+                                print!("{}", *i);
+                                print!("{}", filter.red());
+                            }
+                            println!("{}", phrases[phrases.len() - 1]);
+                        }
+                    }
+                } else {
+                    // Display message informing that the file size reduced since the last output. continue following new EOF
+                    println!("***FILE TRUNCATED: READING FROM NEW EOF***");
+                }
+
+                // update self.size after we are done printing/filtering
+                self.update_size();
+            }
+            Input::Stdin => {}
+        }
     }
 }
 
 fn main() {
     let mut args = Args::parse();
 
-    let filepath = to_str(args.file);
+    let input = match args.file {
+        Some(file) => {
+            let filepath = to_str(file);
 
-    let path = if filepath != "" {
-        Some(Path::new(filepath))
-    } else {
-        None
+            let p = Path::new(filepath);
+
+            match p.try_exists() {
+                Ok(true) => Input::File(p),
+                Ok(false) => {
+                    eprintln!("error: file doesn't exist");
+                    process::exit(-1);
+                }
+                Err(e) => {
+                    eprintln!("{}", e);
+                    process::exit(-1);
+                }
+            }
+        }
+        None => Input::Stdin,
     };
 
-    let input = std::io::stdin();
-
-    let stdin_lines: Option<Vec<String>> = if !input.is_terminal() {
-        Some(input.lines().collect::<Result<Vec<_>, _>>().unwrap())
-    } else {
-        None
-    };
-
+    let mut fspec = FileSpec::new(input);
     let sieve = to_str(args.sieve);
 
     // Automatically follow if sieve is specified
@@ -68,111 +173,28 @@ fn main() {
         args.follow = true;
     }
 
-    let mut fspec = FileSpec::new(path, stdin_lines);
+    let num = match args.num_lines.parse::<i32>() {
+        Ok(v) => v,
+        Err(_) => {
+            eprintln!("illegal offset: {}", args.num_lines);
+            std::process::exit(-1);
+        }
+    };
 
-    let num = args.num_lines.parse::<i32>().unwrap();
-    read_last_n_lines(&mut fspec, num);
+    fspec.read_last_n_lines(num);
 
-    if args.follow && path.is_some() {
-        let mut watcher = notify::recommended_watcher(move |res| match res {
-            Ok(_event) => follow_filter(&mut fspec, sieve),
-            Err(e) => println!("watch error: {:?}", e),
-        })
-        .unwrap();
-
-        watcher
-            .watch(path.unwrap(), RecursiveMode::Recursive)
-            .unwrap();
-        loop {}
-    }
-}
-
-fn read_last_n_lines(file: &mut FileSpec, num: i32) {
-    let mut b_ns = num;
-    let mut start: u64 = 0;
-
-    if file.fpath.is_some() {
-        let mut f = File::options()
-            .read(true)
-            .write(false)
-            .open(file.fpath.unwrap())
+    if let Input::File(path) = fspec.input {
+        if args.follow {
+            let mut watcher = notify::recommended_watcher(move |res| match res {
+                Ok(_event) => fspec.follow_filter(sieve),
+                Err(e) => println!("watch error: {:?}", e),
+            })
             .unwrap();
 
-        // Stop when number of \n are met, or the file is completely read.
-        while b_ns > 0 && start < file.size {
-            // Read one byte at a time until we reach the specified number of \n's (b_ns)
-            start = start + 1;
-
-            f.seek(SeekFrom::Start(file.size - start)).unwrap();
-
-            let mut buf = vec![0; 1];
-            f.read_exact(&mut buf).unwrap();
-
-            if from_utf8(&buf).unwrap() == "\n".to_string() {
-                b_ns -= 1;
-            }
-        }
-
-        // Seek to position of last \n and print rest of the file out.
-        f.seek(SeekFrom::Start(file.size - start)).unwrap();
-
-        let mut buf_print = Vec::new();
-        f.read_to_end(&mut buf_print).unwrap();
-
-        print!("{}", String::from_utf8(buf_print).unwrap());
-    } else if file.stdin.is_some() {
-        // Far easier to do when input is stdin string...
-
-        let mut buf_print = String::new();
-
-        for lines in file.stdin.clone().unwrap().iter().rev() {
-            buf_print.insert_str(0, &format!("{}\n", lines)[..]);
-            b_ns -= 1;
-            if b_ns == 0 {
-                break;
-            }
-
-            print!("{}", buf_print);
+            watcher.watch(path, RecursiveMode::Recursive).unwrap();
+            loop {}
         }
     }
-}
-
-fn follow_filter(file: &mut FileSpec, filter: &str) {
-    if file.fpath.unwrap().metadata().unwrap().len() >= file.size {
-        // Regular tail -f behaviour so far.
-        let mut f = File::options()
-            .read(true)
-            .write(false)
-            .open(file.fpath.unwrap())
-            .unwrap();
-
-        // delay updating file.size so that we know exact seek of where the last output ended
-        f.seek(SeekFrom::Start(file.size)).unwrap();
-
-        let mut buf = Vec::new();
-        f.read_to_end(&mut buf).unwrap();
-        let new_line = String::from_utf8(buf).unwrap();
-
-        // Start filtering things out here...
-        let lines = new_line.split("\n");
-
-        for line in lines {
-            if line.contains(&filter) {
-                let phrases: Vec<&str> = line.split(filter).collect();
-                for i in phrases[..phrases.len() - 1].iter() {
-                    print!("{}", *i);
-                    print!("{}", filter.red());
-                }
-                println!("{}", phrases[phrases.len() - 1]);
-            }
-        }
-    } else {
-        // Display message informing that the file size reduced since the last output. continue following new EOF
-        println!("***FILE TRUNCATED: READING FROM NEW EOF***");
-    }
-
-    // update file.size after we are done printing/filtering
-    file.update_size();
 }
 
 #[derive(Parser, Debug, Clone)]
@@ -191,6 +213,6 @@ struct Args {
     num_lines: String,
 
     /// Path of the file to tail/follow.
-    #[arg(default_value = "")]
-    file: String,
+    #[arg(default_value = None)]
+    file: Option<String>,
 }
